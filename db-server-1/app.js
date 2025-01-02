09f900e1287a08c38e4844b8b77969673e4b3360
alihakhan220 <alihakhan220@gmail.com>
2025-01-02 21:15:02 +0500

    reverting

--- db-server-1/app.js
+++ db-server-1/app.js
@@ -8,19 +8,17 @@ const logger = require("./logger");
 dotenv.config();
 const app = express();
 
-// app.use(cors({
-//   origin: '*',  
-//   methods: ['GET', 'POST', 'PUT'],
-//   allowedHeaders: ['Content-Type', 'Authorization'],
-//   credentials: true, 
-// }));
-const corsConfig = {
-  origin: ["*"],
-}
+app.use(cors({
+  origin: '*',  
+  methods: ['GET', 'POST', 'PUT'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+  credentials: true, 
+}));
+
 
 app.use(express.json()); 
 
-app.use(cors(corsConfig))
+
 
 
 app.use("/application", applicationRoutes);
