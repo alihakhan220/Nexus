68dd3b4aa6329f0c328f01eb97ca8c1833fa6d0f
alihakhan220 <alihakhan220@gmail.com>
2025-01-02 20:57:44 +0500

    cors

--- db-server-1/app.js
+++ db-server-1/app.js
@@ -8,14 +8,20 @@ const logger = require("./logger");
 dotenv.config();
 const app = express();
 
-app.use(cors({
-  origin: '*',  
-  methods: ['GET', 'POST', 'PUT'],
-  allowedHeaders: ['Content-Type', 'Authorization'],
-  credentials: true, 
-}));
+// app.use(cors({
+//   origin: '*',  
+//   methods: ['GET', 'POST', 'PUT'],
+//   allowedHeaders: ['Content-Type', 'Authorization'],
+//   credentials: true, 
+// }));
+const corsConfig = {
+  origin: ["*"],
+}
 
 app.use(express.json()); 
+
+app.use(cors())
+
 
 app.use("/application", applicationRoutes);
 
