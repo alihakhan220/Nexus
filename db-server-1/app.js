c0a12d6e6883f9b988599acd975c63c5a577ae6f
alihakhan220 <alihakhan220@gmail.com>
2025-01-03 01:11:15 +0500

    cors

--- db-server-1/app.js
+++ db-server-1/app.js
@@ -9,7 +9,7 @@ dotenv.config();
 const app = express();
 
 app.use(cors({
-  origin: 'http://localhost:5173',  
+  origin: 'https://fronte-60xb.onrender.com',  
   methods: ['GET', 'POST', 'PUT'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true, 
