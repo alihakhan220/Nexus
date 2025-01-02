11e3fe5aeb8f07bae32604c2d68db3b809e9494c
alihakhan220 <alihakhan220@gmail.com>
2025-01-03 00:39:13 +0500

    build

--- Nexus/vite.config.js
+++ Nexus/vite.config.js
@@ -3,6 +3,9 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  build: {
+    outDir: 'dist', // This is the default
+  },
   plugins: [react()],
   server: {
     proxy: {
